831d1c220f2fd3c71c33d53103a0b7bef2099431
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-06 19:03:09 -0500

    fix setAttribute for SVG elements

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 require("good-dom").global()
 
+// const svgElementNames = Set(["a","animate","animateMotion","animateTransform","circle","clipPath","defs","desc","discard","ellipse","feBlend","feColorMatrix","feComponentTransfer","feComposite","feConvolveMatrix","feDiffuseLighting","feDisplacementMap","feDistantLight","feDropShadow","feFlood","feFuncA","feFuncB","feFuncG","feFuncR","feGaussianBlur","feImage","feMerge","feMergeNode","feMorphology","feOffset","fePointLight","feSpecularLighting","feSpotLight","feTile","feTurbulence","filter","foreignObject","g","hatch","hatchpath","image","line","linearGradient","marker","mask","mesh","meshgradient","meshpatch","meshrow","metadata","mpath","path","pattern","polygon","polyline","radialGradient","rect","script","set","stop","style","svg","switch","symbol","text","textPath","title","tspan","unknown","use","view"])
 // create a JSX middleware system if it doesnt exist
 if (!window.jsxChain) {
     window.jsxChain = []
 }
 
 let isConstructor = (obj) => {
-  return !!obj.prototype && !!obj.prototype.constructor.name;
+    return !!obj.prototype && !!obj.prototype.constructor.name;
 }
 
 // add it to JSX
 window.React = {
-    createElement: (name, properties, ...children) => {
+    createElement: (key, properties, ...children) => {
         // run middleware
         for (let eachMiddleWare of window.jsxChain) {
-            let element = eachMiddleWare(name, properties, ...children)
+            const element = eachMiddleWare(key, properties, ...children)
             if (element) {
                 return element
             }
         }
-        if (name instanceof Function) {
-            if (isConstructor(name)) {
-                return new name;
+        if (key instanceof Function) {
+            if (isConstructor(key)) {
+                return new key({...properties, children})
             } else {
-                return name({...properties, children: children})
+                return key({...properties, children: children})
             }
         }
-        return Object.assign(document.createElement(name), properties).add(...children)
+        const element = document.createElement(key)
+        for (const [key, value] of Object.entries(properties)) {
+            try {
+                element.setAttribute(key, value)
+            } catch (error) {
+                element[key] = value
+            }
+        }
+        return element.add(...children)
     },
 }
